2293853c5b2474555ead88d120c099b9761da0e6
Jean Carlos Santoya Cabrera <jeancarlosodi@gmail.com>
2020-07-02 09:00:16 -0500

    added basic print in cpp

---
+++ 2-print/print.cpp
@@ -0,0 +1,14 @@
+# include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main(){
+
+    int age = 19;
+    int birth = 2001;
+
+    cout << "Pepe have " << age << " years old." << endl;
+
+    printf("He birth in %i\n", birth);
+}
