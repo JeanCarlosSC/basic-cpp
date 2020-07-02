22c0402d18a610c06fd3c5b278a278949c4d022b
Jean Carlos Santoya Cabrera <jeancarlosodi@gmail.com>
2020-07-02 09:09:01 -0500

    added console in, in cpp

---
+++ 3-cin/cin.cpp
@@ -0,0 +1,12 @@
+# include <iostream>
+
+using namespace std;
+
+int main(){
+    int number;
+
+    cout << "What is your favorite number?" << endl;
+    cin >> number;
+
+    cout << "Your favorite number is " << number << endl;
+}
